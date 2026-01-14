ea331df756a0ecdf3fdbe8ffe0e8a3d36206a29e
hichamlahlou <hlahlou@frepple.com>
2026-01-14 23:13:37 +0100

    add a tooltip to the recommendation column

--- frepple/__manifest__.py
+++ frepple/__manifest__.py
@@ -34,6 +34,7 @@
     "assets": {
         "web.assets_backend": [
             "frepple/static/src/js/frepple.js",
+            "frepple/static/src/js/html_tooltip_widget.js",
             "frepple/static/src/js/frepple_recommendation_banner.js",
             "frepple/static/src/xml/frepple_recommendation_banner.xml",
         ],
