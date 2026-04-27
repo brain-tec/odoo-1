2cd546f67ae31e18aca61c7fb70896a949f80702
hichamlahlou <hlahlou@frepple.com>
2026-04-27 15:49:39 +0200

    add dialog when generating the recommendations to choose the planning constraints and the company

---
+++ frepple/static/src/js/frepple_launch_dialog.js
@@ -0,0 +1,53 @@
+/** @odoo-module **/
+import { Component, useState } from "@odoo/owl";
+import { Dialog } from "@web/core/dialog/dialog";
+
+export class FreppleLaunchDialog extends Component {
+  static template = "frepple.LaunchDialog";
+  static components = { Dialog };
+  static props = {
+    companies: { type: Array },
+    defaultCompanyId: { type: Number },
+    close: { type: Function },
+    onConfirm: { type: Function },
+  };
+
+  setup() {
+    this.state = useState({
+      capacity: true,
+      mfgLeadTime: true,
+      poLeadTime: true,
+      companyId: this.props.defaultCompanyId,
+    });
+  }
+
+  onConfirm() {
+    this.props.onConfirm({
+      capacity: this.state.capacity,
+      mfgLeadTime: this.state.mfgLeadTime,
+      poLeadTime: this.state.poLeadTime,
+      companyId: this.state.companyId,
+    });
+    this.props.close();
+  }
+
+  onCancel() {
+    this.props.close();
+  }
+
+  onCapacityChange(ev) {
+    this.state.capacity = ev.target.checked;
+  }
+
+  onMfgLeadTimeChange(ev) {
+    this.state.mfgLeadTime = ev.target.checked;
+  }
+
+  onPoLeadTimeChange(ev) {
+    this.state.poLeadTime = ev.target.checked;
+  }
+
+  onCompanyChange(ev) {
+    this.state.companyId = parseInt(ev.target.value);
+  }
+}
