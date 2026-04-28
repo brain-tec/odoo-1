fcd0684af675eba5a492deb3f91c15da097a0b59
hichamlahlou <hlahlou@frepple.com>
2026-04-28 15:09:36 +0200

    add message to the "frepple aps" link when connected to an anonymous server

--- frepple/models/res_company.py
+++ frepple/models/res_company.py
@@ -82,7 +82,16 @@ class ResCompany(models.Model):
     @api.model
     def actionOpenFreppleWindow(self):
         current_company = self.env.company
-        frepple_url = current_company.getFreppleURL(True, "/")
+
+        server = (current_company.frepple_server or "").rstrip("/").lower()
+        if server == "https://odoo.frepple.com":
+            return {
+                "type": "ir.actions.client",
+                "tag": "frepple.anonymous_server_info",
+                "name": "FrePPLe APS",
+            }
+
+        frepple_url = current_company.getFreppleURL(True, "/", True)
 
         if not frepple_url:
             # If the URL is missing, return a notification action
